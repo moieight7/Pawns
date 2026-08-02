d5551dd852939a970e9df03d927a942b02cdc530
moieight7 <moieight7@gmail.com>
2026-08-02 16:12:55 +0200

    The archer's ricochet no longer bounces off of a non-existent wall upon spawning

--- Assets/Scripts/RicochetProjectile.cs
+++ Assets/Scripts/RicochetProjectile.cs
@@ -8,16 +8,18 @@ public class RicochetProjectile : Projectile
     public int numberOfBounces = 3;
     public LayerMask bounceOffMask;
 
-    private Vector3 previousPosition;
+    private Vector3 previousPosition = Vector3.zero;
     private Vector3 currentPositionRayTo;
 
     private void FixedUpdate()
     {
+        if (previousPosition == Vector3.zero) previousPosition = transform.position;
         currentPositionRayTo = (transform.position - previousPosition);
 
         RaycastHit2D hit = Physics2D.Raycast(previousPosition, currentPositionRayTo, Vector3.Distance(transform.position, previousPosition), bounceOffMask);
         if (hit.collider != null)
         {
+            Debug.Log("Bounce off of " + hit.collider.name + " distance: " + Vector3.Distance(transform.position, previousPosition));
             numberOfBounces--;
             if (numberOfBounces <= 0) Destroy(gameObject);
 
@@ -27,22 +29,5 @@ public class RicochetProjectile : Projectile
             SetDirection(rb.velocity.normalized, rotationZ);
         }
         previousPosition = rb.position;
-    }
-
-    protected new void OnTriggerEnter2D(Collider2D collision)
-    {
-        base.OnTriggerEnter2D(collision);
-
-        /*RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 10, bounceOffMask);
-        if (hit.collider != null)
-        {
-            numberOfBounces--;
-            if (numberOfBounces <= 0) Destroy(gameObject);
-
-            rb.velocity = Vector2.Reflect(rb.velocity.normalized, hit.normal) * speed;
-            float rotationZ = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
-            //transform.rotation = Quaternion.AngleAxis(rotationZ, Vector3.forward);
-            SetDirection(rb.velocity.normalized, rotationZ);
-        }*/
     }
 }
